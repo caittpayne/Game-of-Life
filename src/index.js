1a3ee594be462c2cbbfe705eebb1ecf8d99de31c
Cait Payne <caitpayne@outlook.com>
2018-12-18 15:43:02 -0700

    game of life complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,141 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Box extends React.Component {
+    selectBox = () => {
+        this.props.selectBox(this.props.row, this.props.col)
+    }
+    render() {
+        return (
+            <div
+                className={this.props.boxClass}
+                id={this.props.id}
+                onClick={this.selectBox}
+            />
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Grid extends React.Component {
+    render() {
+        const width = this.props.cols * 16;
+        var rowsArr = [];
+
+        var boxClass = '';
+
+        for(var i = 0; i < this.props.rows; i++) {
+            for(var j = 0; j < this.props.cols; j++) {
+                let boxId = i + '' + j;
+
+                boxClass = this.props.gridFull[i][j] ? 'box on' : 'box off';
+                rowsArr.push(
+                    <Box
+                        boxClass={boxClass}
+                        key={boxId}
+                        boxId={boxId}
+                        row={i}
+                        col={j}
+                        selectBox={this.props.selectBox}
+                    />
+                )
+            }
+        }
+        return (
+            <div className='grid' style={{width: width}}>
+                {rowsArr}
+            </div>
+        )
+    }
+}
+class Main extends React.Component {
+    constructor() {
+        super();
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
+        this.state = {
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+        }
+    }
+
+    selectBox = (row, col) => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        gridCopy[row][col] = !gridCopy[row][col];
+        this.setState({
+            gridFull: gridCopy
+        })
+    }
+
+    seed = () => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        for(let i = 0; i < this.rows; i++) {
+            for(let j = 0; j < this.cols; j++) {
+                if(Math.floor(Math.random() * 2) === 1) {
+                    gridCopy[i][j] = true;
+                }
+            }
+        }
+        this.setState({
+            gridFull: gridCopy
+        });
+    }
+
+    startGame = () => {
+        this.intervalId = setInterval(this.play, this.props.speed);
+        this.intervalId = setInterval(this.play, this.speed);
+    }
+
+    play = () => {
+        let g = this.state.gridFull;
+        let g2 =arrayClone(this.state.gridFull);
+
+        // rules
+
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+              let count = 0;
+              if (i > 0) if (g[i - 1][j]) count++;
+              if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+              if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+              if (j < this.cols - 1) if (g[i][j + 1]) count++;
+              if (j > 0) if (g[i][j - 1]) count++;
+              if (i < this.rows - 1) if (g[i + 1][j]) count++;
+              if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+              if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+              if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+              if (!g[i][j] && count === 3) g2[i][j] = true;
+            }
+          }
+          this.setState({
+            gridFull: g2,
+            generation: this.state.generation + 1
+          });
+    }
+
+    componentDidMount() {
+        this.seed();
+        this.startGame();
+    }
+    render() {
+        return (
+            <div>
+                <h1>The Game of Life</h1>
+                <Grid
+                    gridFull={this.state.gridFull}
+                    rows={this.rows}
+                    cols={this.cols}
+                    selectBox={this.selectBox}
+                />
+            </div>
+        );
+    }
+}
+
+function arrayClone(arr) {
+    return JSON.parse(JSON.stringify(arr));
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+
+
